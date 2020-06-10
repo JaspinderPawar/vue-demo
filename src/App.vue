cd578220bbda54c282f54194ab7b621300c37f56
JaspinderPawar <jaspinder@ucreate.co.in>
2020-06-10 16:02:26 +0530

    Add code to repo

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  <Header/> 
+  <div>
+    Hello world
+  </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
